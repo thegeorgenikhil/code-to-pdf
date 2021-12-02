6fa0de26b257bd20149dad6d99ff1d23a4fc4dd0
Nikhil George <56214901+thegeorgenikhil@users.noreply.github.com>
2021-12-02 20:37:15 +0530

    add✅: support for code output img + txt file with questions

--- src/index.js
+++ src/index.js
@@ -11,17 +11,46 @@ const MarkdownContent = (
     file_list
 ) => {
     const header = `# ${assignment_name}\n`;
-    const title = `Name: ${student_name} \n\n Roll Number: ${roll_number}\n\n`;
+    const title = `### Name: ${student_name} \n ### Roll Number: ${roll_number}\n---\n\n`;
 
     let program_content = "";
-
+    const ifTextFileExists = prompt("Does it have a text file?(y/n): "); //for assignments with a text file of questions
+    if (ifTextFileExists.toLowerCase() === "y") {
+        for (let i = 0; i < file_list.length; i++) {
+            const file_name = `${dir_name}/${file_list[i]}`;
+            const file_extension = file_name.substring(file_name.length - 1);
+            if (file_extension === "t") { //checks for .txt file
+                const data = fs.readFileSync(`${dir_name}/${file_list[i]}`);
+                program_content += `# Questions:`;
+                program_content += "\n";
+                program_content += data;
+                program_content += "\n";
+                program_content += `<div class="page-break"></div>`;
+                program_content += "\n\n";
+            }
+        }
+       
+    }
+    let questionCount = 1;
     for (let i = 0; i < file_list.length; i++) {
-        const data = fs.readFileSync(`${dir_name}/${file_list[i]}`);
-
-        program_content += `### Q. ${i + 1} \n`;
-        program_content += "```\n";
-        program_content += data;
-        program_content += "\n```\n";
+        const file_name = `${dir_name}/${file_list[i]}`;
+        const file_extension = file_name.substring(file_name.length - 1);
+        if (file_extension === "c") { //checks for .c file
+            const data = fs.readFileSync(`${dir_name}/${file_list[i]}`);
+            program_content += `# Q. ${questionCount} \n\n`;
+            questionCount = questionCount + 1;
+            program_content += "```\n";
+            program_content += data;
+            program_content += "\n```\n";
+        }else if (file_extension === "g") { //checks for .png/.jpeg file
+            program_content += `### Output:`;
+            program_content += "\n";
+            program_content += `![img${i}](${file_name})`; //img path
+            if (i !== file_list.length - 2) {
+                program_content += `<div class="page-break"></div>`;
+                program_content += "\n";
+            }
+        }
     }
 
     return `${header} ${title} ${program_content}`;
@@ -38,7 +67,7 @@ class CodeToPdfCommand extends Command {
             fs.readdir(dir, async (err, files) => {
                 const assignment_name = prompt("Assignment Name: ");
                 const name = prompt("Enter Name: ");
-                const roll_no = prompt("Enter Roll Number: ");
+                const roll_no = prompt("Enter Roll No.: ");
 
                 const md = MarkdownContent(
                     assignment_name,
@@ -49,11 +78,13 @@ class CodeToPdfCommand extends Command {
                 );
 
                 fs.writeFileSync(`${output}.md`, md);
-                	
-				const pdf = await mdToPdf({ path: `${output}.md` }).catch(console.error);
-				if (pdf) {
-					fs.writeFileSync(`${output}.pdf`, pdf.content)
-				}
+
+                const pdf = await mdToPdf({ path: `${output}.md` }).catch(
+                    console.error
+                );
+                if (pdf) {
+                    fs.writeFileSync(`${output}.pdf`, pdf.content);
+                }
             });
         } else {
             this.log("Bruh, mention a directory");
